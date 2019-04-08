c79fee5cd7e4f5b303ff37511495eb42b7b3e874
Chris Y Cho <chrisycho04@gmail.com>
2019-04-07 23:58:51 -0700

    heroku push

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server is up and running."))
