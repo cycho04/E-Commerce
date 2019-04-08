902aaadd18489a8c1e59a5c173b72b0ee436fc26
Chris Y Cho <chrisycho04@gmail.com>
2019-04-08 01:09:22 -0700

    full stack app deployed on heroku. prototype

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
 const PORT = process.env.PORT || 5000;
