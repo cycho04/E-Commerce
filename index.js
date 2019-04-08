37f6cd54e7931d78c3cc7fdc77bfb3c0d9a24e8a
Chris Y Cho <chrisycho04@gmail.com>
2019-04-08 00:16:35 -0700

    heroku launch

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/', express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', (req, res) => {
     res.send('Home');
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server is up and running."))
