f417c683751b4e1f28f668cbd7a6455dceae81a7
Chris Y Cho <chrisycho04@gmail.com>
2019-04-08 23:51:24 -0700

    adding OAuth

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const mongoose = require('mongoose');
+require('./routes/authRoutes')(app);
 
-app.use(bodyParser.json());
-app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, '/client/build')));
 
-app.get('/', (req, res) => {
-    res.send('Home');
-});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-})
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server is up and running."))
